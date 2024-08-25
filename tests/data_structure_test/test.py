cfc8a02599ccacf425412a631ce1d38a0da39ef6
ninja <astore.a.fabio@gmail.com>
2024-08-25 15:17:55 -0300

    data

--- tests/data_structure_test/test.py
+++ tests/data_structure_test/test.py
@@ -1,13 +1,14 @@
-from typing import NoReturn
-import unittest
-from tests.data_structure_test.test import DataStructureTest
 from tests.data_structure_test.fdnode import FDNodeTest
 from tests.data_structure_test.node import NodeTest
 from tests.data_structure_test.multidimensional_node import MultidimensionalNodeTest
+from typing import NoReturn
+import unittest
 
-class Test(unittest.TestCase):
+class DataStructureTest(unittest.TestCase):
     def __init__(self) -> NoReturn:
-        DataStructureTest()
+        FDNodeTest()
+        NodeTest()
+        MultidimensionalNodeTest()
 
 if __name__ == "__main__":
-    Test()
+    DataStructureTest()
